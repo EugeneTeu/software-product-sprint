61aad57f68e0a134fbdefdd586944231c26f05d6
Eugene Teu <eugene.teu@shopee.com>
2020-06-12 15:47:09 +0800

    fix typo

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -21,7 +21,7 @@ public final class Comment {
   private final String text;
   private final long timestamp;
 
-  public Task(long id, String text, long timestamp) {
+  public Comment(long id, String text, long timestamp) {
     this.id = id;
     this.text= text;
     this.timestamp = timestamp;
