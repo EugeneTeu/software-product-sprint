9abf3e2d772b71d4348c8a5051311dab44241ff4
Eugene Teu <eugene.teu@shopee.com>
2020-06-19 19:27:50 +0800

    add upload handler

--- portfolio/src/main/java/com/google/sps/servlets/BlobstoreUploadUrlServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BlobstoreUploadUrlServlet.java
@@ -31,7 +31,7 @@ public class BlobstoreUploadUrlServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-    String uploadUrl = blobstoreService.createUploadUrl("/my-form-handler");
+    String uploadUrl = blobstoreService.createUploadUrl("/upload-handler");
 
     response.setContentType("text/html");
     response.getWriter().println(uploadUrl);
